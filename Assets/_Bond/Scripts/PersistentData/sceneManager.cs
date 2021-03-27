99f4261859e97dee4a5f40fc3567d37c3fdfde99
Colin Watterson <ctwatterson123@gmail.com>
2021-03-27 16:26:13 -0700

    cleaned up old scenes and terrains and organized base folder

---
+++ Assets/_Bond/Scripts/PersistentData/sceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+    public int sceneIndex;
+    private void OnTriggerEnter(Collider other) 
+    {
+        // Debug.Log("entered");
+        if(other.transform.tag == "Player")
+        {
+            // Debug.Log("try load");
+            PersistentData.Instance.LoadScene(sceneIndex);
+        } 
+    }
+}
