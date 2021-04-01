bde2c672adaf6965b5f4be18e79fa0d8b8287a8d
Jameson <jdanning@ucsc.edu>
2021-03-31 17:22:26 -0700

    enthusiasm tweaks

--- Assets/_Bond/Scripts/UI/EnthusiasmUI.cs
+++ Assets/_Bond/Scripts/UI/EnthusiasmUI.cs
@@ -8,6 +8,7 @@ public class EnthusiasmUI : MonoBehaviour
     private PlayerController player => PersistentData.Instance.Player.GetComponent<PlayerController>();
     public Image enthusiasm;
 
+    
     private void FixedUpdate() 
     {
         enthusiasm.transform.rotation = Camera.main.transform.rotation;
@@ -16,6 +17,6 @@ public class EnthusiasmUI : MonoBehaviour
     public void UpdateEnthusiasm()
     {
         var creatureStats = player.currCreatureContext.creatureStats.statManager;
-        enthusiasm.fillAmount = ((creatureStats.getStat(ModiferType.CURR_ENTHUSIASM) / creatureStats.getStat(ModiferType.MAX_ENTHUSIASM)) * 100);
+        enthusiasm.fillAmount = ((creatureStats.getStat(ModiferType.CURR_ENTHUSIASM) / creatureStats.getStat(ModiferType.MAX_ENTHUSIASM)));
     }
 }
