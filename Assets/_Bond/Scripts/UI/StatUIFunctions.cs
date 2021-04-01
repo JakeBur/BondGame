88b2beab6d766a36e5ffefcbd2c27f65d62406ba
Jameson <jdanning@ucsc.edu>
2021-04-01 16:57:01 -0700

    Auto stash before merge of "JamoUIFunctional" and "origin/main"

---
+++ Assets/_Bond/Scripts/UI/StatUIFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatUIFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Player Stats")]
+    public TextMeshProUGUI lifeText;
+    public TextMeshProUGUI PowerText;
+    public TextMeshProUGUI BondText;
+    public TextMeshProUGUI CritText;
+    public TextMeshProUGUI SpeedText;
+
+
+    [Header("Creature Stats")]
+    public TextMeshProUGUI CreaturelifeText;
+    public TextMeshProUGUI CreaturePowerText;
+    public TextMeshProUGUI CreatureDexterityText;
+    public TextMeshProUGUI CreatureUtilityText;
+    public TextMeshProUGUI CreatureBehaviorText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
