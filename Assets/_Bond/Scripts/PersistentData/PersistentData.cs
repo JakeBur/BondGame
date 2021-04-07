a05f355ab077348731bfbaf390d9a2bd47796398
Colin Watterson <ctwatterson123@gmail.com>
2021-04-06 17:58:17 -0700

    relic shop ui

--- Assets/_Bond/Scripts/PersistentData/PersistentData.cs
+++ Assets/_Bond/Scripts/PersistentData/PersistentData.cs
@@ -30,6 +30,11 @@ public class PersistentData : MonoBehaviour
     public GameObject StatUI { get; private set;}
     private GameObject statUI;
 
+    [Header("ShopRelicUIReference")]
+    public GameObject ShopRelicUIPrefab;
+    public GameObject ShopRelicUI { get; private set;}
+    private GameObject shopRelicUI;
+
     [Header("AudioReference")]
     public GameObject AudioControllerPrefab;
     public GameObject AudioController {get; private set;}
@@ -38,6 +43,8 @@ public class PersistentData : MonoBehaviour
     public CanvasGroup loadScreen;
 
     public bool isGeneratorDone;
+
+    public List<RelicStats> availableRelics;
 
 
     private void Awake() 
@@ -74,6 +81,28 @@ public class PersistentData : MonoBehaviour
             
         }
         Camera.main.GetComponent<CamFollow>().toFollow = Player.transform;
+
+
+
+        if(ShopRelicUI == null)
+        {
+            try
+            {
+                ShopRelicUI = GameObject.FindGameObjectWithTag("ShopRelicUI");
+                if(ShopRelicUI == null)
+                {
+                    ShopRelicUI = Instantiate(ShopRelicUIPrefab, Vector3.zero, Quaternion.identity);
+                    ShopRelicUI.SetActive(false);
+                }
+                
+            }
+            catch
+            {
+                ShopRelicUI = Instantiate(ShopRelicUIPrefab, Vector3.zero, Quaternion.identity);
+                ShopRelicUI.SetActive(false);
+            }
+            
+        }
 
 
 
@@ -183,6 +212,7 @@ public class PersistentData : MonoBehaviour
         MakeChild(UI);
         MakeChild(PauseMenu);
         MakeChild(StatUI);
+        MakeChild(ShopRelicUI);
         //Loading Scene, can make transition stuff here
          //for example, some screen fading stuff : 
             //transition OUT
@@ -204,6 +234,7 @@ public class PersistentData : MonoBehaviour
         UnmakeChild(UI);
         UnmakeChild(PauseMenu);
         UnmakeChild(StatUI);
+        UnmakeChild(ShopRelicUI);
 
         //set players position in new scene
         //CALL BUILD LEVEL, WHICH SHOULD GENERATE EVERYTHING, INCLUDING A SPAWNPOINT;
