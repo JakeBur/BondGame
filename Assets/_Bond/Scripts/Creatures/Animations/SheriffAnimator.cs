ea03fab199cab45cb6488804f269a11773b2e77a
Rookirk <hwu24110@gmail.com>
2021-05-19 18:07:43 -0700

    Set up Sheriff animator stuff and freezing movement

---
+++ Assets/_Bond/Scripts/Creatures/Animations/SheriffAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheriffAnimator : CreatureAnimator
+{
+    public void BicycleKick()
+    {
+        animator.SetTrigger("BicycleKick");
+    }
+
+    public void ElusiveStepPose()
+    {
+        animator.SetTrigger("ElusiveStepPose");
+    }
+
+    public void ElusiveStepStrike()
+    {
+        animator.SetTrigger("ElusiveStepStrike");
+    }
+
+    public void Quickdraw()
+    {
+        animator.SetTrigger("Quickdraw");
+    }
+
+    public void Shotgun()
+    {
+        animator.SetTrigger("Shotgun");
+    }
+}
