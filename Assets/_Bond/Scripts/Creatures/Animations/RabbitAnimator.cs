cc489e15056b78110b154874113acfa3ce29bc3a
Rookirk <hwu24110@gmail.com>
2021-05-19 18:58:44 -0700

    Renamed Sheriff assets to Rabbit and also did more animator setup

---
+++ Assets/_Bond/Scripts/Creatures/Animations/RabbitAnimator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitAnimator : CreatureAnimator
+{
+    public GameObject kunaiModel;
+    private SkinnedMeshRenderer kunaiMesh => kunaiModel.GetComponent<SkinnedMeshRenderer>();
+
+    public bool inElusiveStepPose { get; protected set; }
+
+    public void EventHideKunai()
+    {
+        kunaiMesh.enabled = false;
+    }
+
+    public void EventShowKunai()
+    {
+        kunaiMesh.enabled = true;
+    }
+
+    public void EventElusiveStepPoseDone()
+    {
+        inElusiveStepPose = false;
+    }
+
+    public void BicycleKick()
+    {
+        animator.SetTrigger("BicycleKick");
+
+        EventHideKunai();
+    }
+
+    public void ElusiveStepPose()
+    {
+        inElusiveStepPose = true;
+
+        animator.SetTrigger("ElusiveStepPose");
+
+        EventShowKunai();
+    }
+
+    public void ElusiveStepStrike()
+    {
+        animator.SetTrigger("ElusiveStepStrike");
+
+        EventShowKunai();
+    }
+
+    public void Quickdraw()
+    {
+        animator.SetTrigger("Quickdraw");
+
+        EventShowKunai();
+    }
+
+    public void Shotgun()
+    {
+        animator.SetTrigger("Shotgun");
+
+        EventShowKunai();
+    }
+}
--- Assets/_Bond/Scripts/Creatures/Animations/SheriffAnimationEvent.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SheriffAnimationEvent : CreatureAnimationEvent
-{
-    protected SheriffAnimator sheriffAnimator => transform.parent.GetComponent<SheriffAnimator>();
-}
